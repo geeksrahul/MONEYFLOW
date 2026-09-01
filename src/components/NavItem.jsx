28b6f5f1bbc7b7eb73b8b0f723feb5ff6fc83fb0
geeksrahul <geeksrahulofficial@gmail.com>
2026-09-01 11:07:39 +0530

    fix : fix navigation tooltip hover behaviour with state

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,24 +1,34 @@
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
-const NavToolTip = ({title}) => (
-  <span className="pointer-event-none absolute left-20 text-white bg-purple-600 py-1 px-3 rounded-md opacity-0 group-hover:opacity-100">
-    {title}
-  </span>
-);
+const NavToolTip = ({title}) => {
+    return (
+        <span className="absolute left-20 text-white bg-purple-600 py-1 px-3 rounded-md">
+            {title}
+        </span>
+    );
+}
 
 const NavItem = ({
     to,
     title, 
     icon 
 }) => {
+    const [tooltip, setTooltip] = useState(false)
     return (
         <NavLink
             to={to}
-            className={({isActive})=>`group relative flex h-12 w-12 items-center justify-center rounded-xl ${isActive ? 'bg-purple-100 text-purple-600 hover:bg-purple-100' : 'bg-transparent text-black hover:bg-gray-100'} `}
+            className={({isActive})=>`relative flex h-12 w-12 items-center justify-center rounded-xl ${isActive ? 'bg-purple-100 text-purple-600 hover:bg-purple-100' : 'bg-transparent text-black hover:bg-gray-100'} `}
+            onMouseEnter={()=>{
+                setTooltip(true)
+            }}
+            onMouseLeave={()=>{
+                setTooltip(false)
+            }}
         >
             <FontAwesomeIcon icon={icon} />
-            <NavToolTip title={title} />
+            {tooltip && <NavToolTip title={title} />}
         </NavLink>
     );
 }
