b75f70add5f4cf007ced2cb69a14b82d38d005d9
geeksrahul <geeksrahulofficial@gmail.com>
2026-09-02 13:55:36 +0530

    refactor: remove obsolete income and expense files (hooks/context)

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,15 +1,9 @@
-import { IncomesContextProvider, useIncomes } from "./incomes";
-import { ExpensesContextProvider, useExpenses } from "./expenses";
 import { CategoriesContextProvider, useCategory } from "./categories";
 import { TransactionContextProvider, useTransaction } from "./transaction"
  
 export {
-    useIncomes,
-    useExpenses,
     useCategory,
     useTransaction,
-    IncomesContextProvider,
-    ExpensesContextProvider,
     CategoriesContextProvider,
     TransactionContextProvider
 }
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,11 +1,7 @@
-import useIncomeData from "./useIncomeData";
-import useExpenseData from "./useExpenseData";
 import useCategoryData from "./useCategoryData";
 import useTransactionData from "./useTransactionData";
 
 export {
-    useIncomeData,
-    useExpenseData,
     useTransactionData,
     useCategoryData
 }
